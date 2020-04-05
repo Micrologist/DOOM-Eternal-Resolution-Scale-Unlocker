c8449696acdf8eb0c0f4ea117363590d27d40c00
Micrologist <mail@davidgalland.de>
2020-04-05 20:50:18 +0200

    fixed binary name

--- obj/x64/Release/MainWindow.g.i.cs
+++ obj/x64/Release/MainWindow.g.i.cs
@@ -68,7 +68,7 @@ namespace DE_ResScale_Unlocker {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DEResScaleUnlocker;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DE ResScale Unlocker;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
